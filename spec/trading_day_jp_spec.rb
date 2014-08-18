908f2d060d3985641bd82408ac2a4f6cf6dedbbb
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-08-19 00:04:58 +0900

    Mountain Day

--- spec/trading_day_jp_spec.rb
+++ spec/trading_day_jp_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe TradingDayJp do
-
   describe '.open?' do
-    it { expect(TradingDayJp.open? Date.new(2014, 8, 28)).to eq true }
-  end
+    context 'given 2014-08-28' do
+      it { expect(TradingDayJp.open? Date.new(2014, 8, 28)).to eq true }
+    end
 
+    context 'given 2016-08-11' do
+      it { expect(TradingDayJp.open? Date.new(2016, 8, 11)).to equal false }
+    end
+  end
 end
