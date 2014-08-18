aced790be5cd6e9cc0ed10af76ebd9604d016ab7
Takehiko Shinkura <tynmarket@gmail.com>
2014-08-18 13:26:15 +0900

    trading_day_jp?をtrading_day_jp/dateに移動

--- lib/trading_day_jp.rb
+++ lib/trading_day_jp.rb
@@ -1,24 +1,5 @@
-require "holiday_jp"
+require "trading_day_jp/date"
 
 module TradingDayJp
-
-  class ::Date
-
-    def trading_day_jp?
-      date_open = Date.new(self.year, 1, 4)
-      date_close = Date.new(self.year, 12, 30)
-
-      if saturday? || sunday?
-        false
-      elsif HolidayJp.holiday? self
-        false
-      elsif self < date_open || self > date_close
-        false
-      else
-        true
-      end
-    end
-
-  end
 
 end
---
+++ lib/trading_day_jp/date.rb
@@ -0,0 +1,20 @@
+require "holiday_jp"
+
+class Date
+
+  def trading_day_jp?
+    date_open = Date.new(self.year, 1, 4)
+    date_close = Date.new(self.year, 12, 30)
+
+    if saturday? || sunday?
+      false
+    elsif HolidayJp.holiday? self
+      false
+    elsif self < date_open || self > date_close
+      false
+    else
+      true
+    end
+  end
+
+end
