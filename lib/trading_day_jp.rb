2f08aba23519283f25235702a4beaafbc459156a
Takehiko Shinkura <tynmarket@gmail.com>
2014-08-18 13:30:03 +0900

    open?追加

--- lib/trading_day_jp.rb
+++ lib/trading_day_jp.rb
@@ -2,4 +2,8 @@ require "trading_day_jp/date"
 
 module TradingDayJp
 
+  def self.open?(date)
+    date.trading_day_jp?
+  end
+
 end
--- spec/trading_day_jp_spec.rb
+++ spec/trading_day_jp_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe TradingDayJp do
 
+  describe '.open?' do
+    it { expect(TradingDayJp.open? Date.new(2014, 8, 28)).to eq true }
+  end
+
 end
