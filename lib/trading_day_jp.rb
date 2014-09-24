42977c0aa11532b00fcce400883b9046cb9e0293
Takehiko Shinkura <tynmarket@gmail.com>
2014-09-24 15:06:06 +0900

    class << self以下にメソッドを定義

--- lib/trading_day_jp.rb
+++ lib/trading_day_jp.rb
@@ -2,106 +2,109 @@ require "trading_day_jp/date"
 
 module TradingDayJp
 
-  def self.open?(date)
-    date.trading_day_jp?
-  end
+  class << self
 
-  def self.next(date)
-    loop do
-      date = date + 1
-
-      return date if date.trading_day_jp?
-    end
-  end
-
-  def self.prev(date)
-    loop do
-      date = date - 1
-
-      return date if date.trading_day_jp?
-    end
-  end
-
-  def self.between(start, last)
-    (start..last).select do |date|
+    def open?(date)
       date.trading_day_jp?
     end
-  end
 
-  def self.beginning_of_month(date)
-    date = Date.new(date.year, date.month)
+    def next(date)
+      loop do
+        date = date + 1
 
-    loop do
-      return date if date.trading_day_jp?
-
-      date = date + 1
+        return date if date.trading_day_jp?
+      end
     end
-  end
 
-  def self.end_of_month(date)
-    date = Date.new(date.year, date.month).next_month - 1
+    def prev(date)
+      loop do
+        date = date - 1
 
-    loop do
-      return date if date.trading_day_jp?
-
-      date = date - 1
+        return date if date.trading_day_jp?
+      end
     end
-  end
 
-  def self.beginning_of_month?(date)
-    date == beginning_of_month(date)
-  end
-
-  def self.end_of_month?(date)
-    date == end_of_month(date)
-  end
-
-  def self.beginning_of_quarter(date)
-    if date.month <= 3
-      beginning_of_month Date.new(date.year, 1)
-    elsif date.month <= 6
-      beginning_of_month Date.new(date.year, 4)
-    elsif date.month <= 9
-      beginning_of_month Date.new(date.year, 7)
-    else
-      beginning_of_month Date.new(date.year, 10)
+    def between(start, last)
+      (start..last).select do |date|
+        date.trading_day_jp?
+      end
     end
-  end
 
-  def self.end_of_quarter(date)
-    if date.month <= 3
-      end_of_month Date.new(date.year, 3)
-    elsif date.month <= 6
-      end_of_month Date.new(date.year, 6)
-    elsif date.month <= 9
-      end_of_month Date.new(date.year, 9)
-    else
-      end_of_month Date.new(date.year, 12)
+    def beginning_of_month(date)
+      date = Date.new(date.year, date.month)
+
+      loop do
+        return date if date.trading_day_jp?
+
+        date = date + 1
+      end
     end
-  end
 
-  def self.beginning_of_quarter?(date)
-    date == beginning_of_quarter(date)
-  end
+    def end_of_month(date)
+      date = Date.new(date.year, date.month).next_month - 1
 
-  def self.end_of_quarter?(date)
-    date == end_of_quarter(date)
-  end
+      loop do
+        return date if date.trading_day_jp?
 
-  def self.beginning_of_year(date)
-    self.next(Date.new date.year)
-  end
+        date = date - 1
+      end
+    end
 
-  def self.end_of_year(date)
-    prev beginning_of_year(Date.new(date.year + 1))
-  end
+    def beginning_of_month?(date)
+      date == beginning_of_month(date)
+    end
 
-  def self.beginning_of_year?(date)
-    date == beginning_of_year(date)
-  end
+    def end_of_month?(date)
+      date == end_of_month(date)
+    end
 
-  def self.end_of_year?(date)
-    date == end_of_year(date)
+    def beginning_of_quarter(date)
+      if date.month <= 3
+        beginning_of_month Date.new(date.year, 1)
+      elsif date.month <= 6
+        beginning_of_month Date.new(date.year, 4)
+      elsif date.month <= 9
+        beginning_of_month Date.new(date.year, 7)
+      else
+        beginning_of_month Date.new(date.year, 10)
+      end
+    end
+
+    def end_of_quarter(date)
+      if date.month <= 3
+        end_of_month Date.new(date.year, 3)
+      elsif date.month <= 6
+        end_of_month Date.new(date.year, 6)
+      elsif date.month <= 9
+        end_of_month Date.new(date.year, 9)
+      else
+        end_of_month Date.new(date.year, 12)
+      end
+    end
+
+    def beginning_of_quarter?(date)
+      date == beginning_of_quarter(date)
+    end
+
+    def end_of_quarter?(date)
+      date == end_of_quarter(date)
+    end
+
+    def beginning_of_year(date)
+      self.next(Date.new date.year)
+    end
+
+    def end_of_year(date)
+      prev beginning_of_year(Date.new(date.year + 1))
+    end
+
+    def beginning_of_year?(date)
+      date == beginning_of_year(date)
+    end
+
+    def end_of_year?(date)
+      date == end_of_year(date)
+    end
   end
 
 end
